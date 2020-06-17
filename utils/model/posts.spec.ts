1f02c028ba7aa5086cc49adefd175b86c1a0ae17
Caleb Lucas <caleb@lucasbin.com>
2020-06-17 22:24:31 +0100

    fix failing test

--- utils/model/posts.spec.ts
+++ utils/model/posts.spec.ts
@@ -2,8 +2,8 @@ import posts, { getPostSummary, getPosts } from "./posts"
 import { markdown01Data } from "../../__tests__/__fixtures__/samplepost";
 
 describe("Posts Model", () => {
-    it("getPostSummary() should return the summary of a post with a fixed length of 120", () => {
-        expect(getPostSummary(markdown01Data)).toHaveLength(120);
+    it("getPostSummary() should return the summary of a post with a fixed length of 110", () => {
+        expect(getPostSummary(markdown01Data)).toHaveLength(110);
     })
 
     it("getPosts() should return a list of posts with the presence of its summary", () => {
