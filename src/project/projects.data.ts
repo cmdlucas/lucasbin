5ced921bce0d02dfa2b18b6657beee76ca402a98
Caleb I. Lucas <caleb@lucasbin.com>
2024-01-27 17:21:54 +0100

    feat: update url & roles

--- src/project/projects.data.ts
+++ src/project/projects.data.ts
@@ -1,13 +1,15 @@
-const SOFTWARE_ENGINEERING = 'Software Engineering' as const;
-const CTO = 'CTO' as const;
-const CO_FOUNDER = 'Co-Founder' as const;
+const Role = {
+  SOFTWARE_ENGINEERING: 'Software Engineering',
+  CTO: 'CTO',
+  CO_FOUNDER: 'Co-Founder',
+} as const;
 
-type Role = typeof SOFTWARE_ENGINEERING | typeof CTO | typeof CO_FOUNDER;
+type Roles = (typeof Role)[keyof typeof Role][];
 
 export interface Project {
   title: string;
   description: string;
-  roles: Role[];
+  roles: Roles;
   linkToProject?: string;
   techStack?: string[];
   desktopScreenshot?: string;
@@ -16,42 +18,42 @@ export interface Project {
 const zubbah: Project = {
   title: 'ZUBBAH.COM (C. 2016 - 2017)',
   description: 'Online marketplace for a wide range of products.',
-  roles: [SOFTWARE_ENGINEERING],
+  roles: [Role.SOFTWARE_ENGINEERING],
   linkToProject: 'https://zubbah.com',
 };
 
 const jolibizAjent: Project = {
   title: 'JOLIBIZ AGENT (C. 2017 - 2019)',
   description: 'Deals management platform for educational consultants.',
-  roles: [SOFTWARE_ENGINEERING],
+  roles: [Role.SOFTWARE_ENGINEERING],
   linkToProject: 'https://jolibiz.com',
 };
 
 const tokeeto: Project = {
   title: 'TOKEETO (C. 2019 - 2020)',
   description: 'Inter-city bus hailing service.',
-  roles: [CO_FOUNDER, CTO],
-  linkToProject: 'https://tokeeto.com',
+  roles: [Role.CO_FOUNDER, Role.CTO],
+  linkToProject: 'https://tinyurl.com/tokeeto-rider',
 };
 
 const jimdo: Project = {
   title: 'JIMDO (C. 2020 - 2022)',
   description: 'Website builder for small businesses.',
-  roles: [SOFTWARE_ENGINEERING],
+  roles: [Role.SOFTWARE_ENGINEERING],
   linkToProject: 'https://jimdo.com',
 };
 
 const mero: Project = {
   title: 'MERO (C. 2021 - 2022)',
   description: 'Property cleaning management.',
-  roles: [SOFTWARE_ENGINEERING],
+  roles: [Role.SOFTWARE_ENGINEERING],
   linkToProject: 'https://mero.co',
 };
 
 const collectAI: Project = {
   title: 'COLLECT AI (C. 2022 - DATE)',
   description: 'Dunning process management for businesses.',
-  roles: [SOFTWARE_ENGINEERING],
+  roles: [Role.SOFTWARE_ENGINEERING],
   linkToProject: 'https://collect.ai',
 };
 
